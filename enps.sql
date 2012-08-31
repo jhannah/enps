251cce3bc766fe53cd03cde2407f2daf144566ac
Jay Hannah <jay.hannah@iinteractive.com>
2012-08-30 19:30:13 -0500

    Parse is now clean. Woot!

---
+++ enps.sql
@@ -0,0 +1,19 @@
+CREATE TABLE results (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   year text,
+   month text,
+   place num, 
+   lname text, 
+   fname text,
+   uspsa text,
+   class text,
+   division text, 
+   pf text,
+   lady text,
+   mil text, 
+   law text, 
+   for text,
+   age text,
+   points num,
+   stgperc num
+);
--- load_db.pl
+++ load_db.pl
@@ -1,6 +1,13 @@
 use strict;
 use 5.10.0;
 
+use DBI;
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=enps.sqlite","","");
+my $strsql = "insert into results (year, month, place, lname, fname, uspsa, class, division, pf, lady, mil, law, for, age, points, stgperc) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+my $sth = $dbh->prepare($strsql);
+
+
 my @files = `find data -name "*.txt"`;
 foreach my $f (@files) {
    chomp $f;
@@ -14,7 +21,7 @@ foreach my $f (@files) {
       if (/^\s+1\s\w/) { $begin = 1; }   # Start reading this file.
       next unless ($begin);              # We're still in the headers. Skip. 
       last unless (/^\s+\d+\s\w/);       # Data has ended. Stop reading this file.
-      say "   $_";
+      # say "   $_";
       my @l = split /\s+/;
       shift @l unless ($l[0]);   # discard first column if empty
 
@@ -34,6 +41,23 @@ foreach my $f (@files) {
          splice @l, -5, 0, 'undef', 'undef';
       }
 
+      # "Limited 10" gets split
+      if ($l[-10] eq "Limited" && $l[-9] eq "10") {
+         $l[-10] = "Limited 10";
+         splice @l, -9, 1;
+      }
+    
+      # "Single Stack" gets split
+      if ($l[-10] eq "Single" && $l[-9] eq "Stack") {
+         $l[-10] = "Single Stack";
+         splice @l, -9, 1;
+      }
+
+      # USPSA "Pen" throws off our logic below
+      if ($l[-11] eq "Pen") {
+         $l[-11] = "PENDING";
+      }
+
       # If USPSA is blank it's undef
       if ($l[-11] =~ /[a-z]/) {
          splice @l, -10, 0, 'undef';
@@ -48,10 +72,9 @@ foreach my $f (@files) {
       # Remove comma from last name.
       $l[1] =~ s/,$//;
 
-      say join "|", @l;
-
-      my ($place, $lname, $fname, $uspsa, $class, $division, $pf, $lady, $for, $age, $points, $stgperc) = @l;
-
+      # say join "|", @l;
+      $sth->execute($y, $m, @l);
+      # my ($place, $lname, $fname, $uspsa, $class, $division, $pf, $lady, $for, $age, $points, $stgperc) = @l;
    }
 }
 
